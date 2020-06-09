715688f48923e25570470f99605cf515173176a0
JPUF <2009bennettj@gmail.com>
2020-06-09 13:19:48 +0100

    Initial commit

---
+++ app/src/main/java/com/jlbennett/flashbotany/flash/FlashFragment.kt
@@ -0,0 +1,29 @@
+package com.jlbennett.flashbotany.flash
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+
+import com.jlbennett.flashbotany.R
+import com.jlbennett.flashbotany.databinding.FragmentFlashBinding
+
+
+class FlashFragment : Fragment() {
+
+    private lateinit var binding: FragmentFlashBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_flash, container, false)
+
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/jlbennett/flashbotany/home/HomeFragment.kt
@@ -0,0 +1,35 @@
+package com.jlbennett.flashbotany.home
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+
+import androidx.navigation.fragment.findNavController
+
+import com.jlbennett.flashbotany.R
+import com.jlbennett.flashbotany.databinding.FragmentHomeBinding
+
+
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        binding.startImage.setOnClickListener { navigateToFlashFragment() }
+
+
+        return binding.root
+    }
+
+    private fun navigateToFlashFragment() {
+        findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToFlashFragment())
+    }
+
+}
